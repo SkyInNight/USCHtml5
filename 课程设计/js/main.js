255bd659cde8bcb977607c17bceed66215c15f52
SkyInNight <1119886064@qq.com>
2019-06-11 00:44:48 +0800

    06110044:增加名人名言特效显现

--- 课程设计/js/main.js
+++ 课程设计/js/main.js
@@ -1,4 +1,19 @@
+var load_in = ['flash', 'bounce', 'shake', 'tada', 'swing', 'wobble', 'pulse', 'flip', 'flipInX', 'flipInY', 'fadeIn', 'fadeInUp', 'fadeInDown', 'fadeInLeft', 'fadeInRight', 'fadeInUpBig', 'fadeInDownBig', 'fadeInLeftBig', 'fadeInRightBig', 'bounceIn', 'bounceInDown', 'bounceInUp', 'bounceInLeft', 'bounceInRight', 'rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight', 'hinge', 'rollIn'];
 $(function () {
+	$('#motto').animatext({
+		speed: 50,
+		effect: load_in[Math.floor(Math.random()*load_in.length)],
+		reverse: false
+	  });
+	$('#motto').everyTime('8s',function(){
+		$.get("https://api.lwl12.com/hitokoto/v1",function(data,state){
+			if (state=="success"){
+				change_motto(data);
+			}else{
+				retur ;
+			}
+		})
+	});
 	$('.tlt').textillate({
 		selector: '.texts',
 		loop: true,
@@ -51,3 +66,11 @@ $('.typing__module').each(function (index) {
 		    options = $.extend(optDefault, optData);
 	var typed = new Typed(_wrapper, options);
 });
+function change_motto(data){
+	$('#motto').text(data);
+	$('#motto').animatext({
+		speed: 50,
+		effect: load_in[Math.floor(Math.random()*load_in.length)],
+		reverse: false
+	  });
+}
